cdeb6f7d5fe14764ab4b2d3277ede2ba998039be
Ubuntu <ubuntu@ip-172-31-41-162.ap-northeast-2.compute.internal>
2019-10-10 02:43:09 +0900

    [Proto] Add Compiler bash script and fix wrong words
    Committer: ATEZ <shieldnet@cau.ac.kr>

--- comfile_protofiles.sh
+++ comfile_protofiles.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+protoc -I=./deal --python_out=.. ./deal/AccountService.proto
 protoc -I=./deal --python_out=.. ./deal/AuthService.proto
-protoc -I=./deal --python_out=.. ./deal/Empty.proto
 protoc -I=./deal --python_out=.. ./deal/CommonResult.proto
+protoc -I=./deal --python_out=.. ./deal/Data.proto
+protoc -I=./deal --python_out=.. ./deal/Date.proto
+protoc -I=./deal --python_out=.. ./deal/Datetime.proto
+protoc -I=./deal --python_out=.. ./deal/DealService.proto
+protoc -I=./deal --python_out=.. ./deal/Empty.proto
+protoc -I=./deal --python_out=.. ./deal/MissionService.proto
+protoc -I=./deal --python_out=.. ./deal/NotificationService.proto
+protoc -I=./deal --python_out=.. ./deal/PhoneService.proto
 protoc -I=./deal --python_out=.. ./deal/PlatformType.proto
+protoc -I=./deal --python_out=.. ./deal/PointService.proto
+protoc -I=./deal --python_out=.. ./deal/UserService.proto
 echo "Compile Done!!"
