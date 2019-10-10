341e7575855fbeadd8f7f97808b584a5499322b2
leesnhyun <leesnhyun@gmail.com>
2019-10-11 02:29:31 +0900

    [Proto] add grpc library for dart script

--- compile_protofile_dart.sh
+++ compile_protofile_dart.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-protoc -I=./deal --dart_out=./gen ./deal/AccountService.proto
-protoc -I=./deal --dart_out=./gen ./deal/AuthService.proto
-protoc -I=./deal --dart_out=./gen ./deal/CommonResult.proto
-protoc -I=./deal --dart_out=./gen ./deal/Data.proto
-protoc -I=./deal --dart_out=./gen ./deal/Date.proto
-protoc -I=./deal --dart_out=./gen ./deal/Datetime.proto
-protoc -I=./deal --dart_out=./gen ./deal/DealService.proto
-protoc -I=./deal --dart_out=./gen ./deal/Empty.proto
-protoc -I=./deal --dart_out=./gen ./deal/MissionService.proto
-protoc -I=./deal --dart_out=./gen ./deal/NotificationService.proto
-protoc -I=./deal --dart_out=./gen ./deal/PhoneService.proto
-protoc -I=./deal --dart_out=./gen ./deal/PlatformType.proto
-protoc -I=./deal --dart_out=./gen ./deal/PointService.proto
-protoc -I=./deal --dart_out=./gen ./deal/UserService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/AccountService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/AuthService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/CommonResult.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/Data.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/Date.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/Datetime.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/DealService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/Empty.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/MissionService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/NotificationService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/PhoneService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/PlatformType.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/PointService.proto
+protoc -I=./deal --dart_out=grpc:./gen ./deal/UserService.proto
 
 echo "Done."
 echo "Please check debug message of command"
