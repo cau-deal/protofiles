d249d72fa52fc4aebd865725562643fe95b52d9e
ATEZ <shieldnet64@gmail.com>
2019-10-12 00:24:12 +0900

    fix *_grpc.py file missing error (comfile shell script)

--- comfile_protofiles.sh
+++ comfile_protofiles.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-protoc -I=./deal --python_out=.. ./deal/AccountService.proto
-protoc -I=./deal --python_out=.. ./deal/AuthService.proto
-protoc -I=./deal --python_out=.. ./deal/CommonResult.proto
-protoc -I=./deal --python_out=.. ./deal/Data.proto
-protoc -I=./deal --python_out=.. ./deal/Date.proto
-protoc -I=./deal --python_out=.. ./deal/Datetime.proto
-protoc -I=./deal --python_out=.. ./deal/DealService.proto
-protoc -I=./deal --python_out=.. ./deal/Empty.proto
-protoc -I=./deal --python_out=.. ./deal/MissionService.proto
-protoc -I=./deal --python_out=.. ./deal/NotificationService.proto
-protoc -I=./deal --python_out=.. ./deal/PhoneService.proto
-protoc -I=./deal --python_out=.. ./deal/PlatformType.proto
-protoc -I=./deal --python_out=.. ./deal/PointService.proto
-protoc -I=./deal --python_out=.. ./deal/UserService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/AccountService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/AuthService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/CommonResult.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/Data.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/Date.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/Datetime.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/DealService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/Empty.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/MissionService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/NotificationService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/PhoneService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/PlatformType.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/PointService.proto
+python3 -m grpc_tools.protoc -I=./deal --python_out=.. --grpc_python_out=.. ./deal/UserService.proto
 echo "Compile Done!!"
 echo "Please check debug message of command"
+
+# python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/route_guide.proto
